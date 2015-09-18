c553cea38fb21de8005360aa82a60fe803af7232
Preston E Libby <plibby@silk1.uvm.edu>
2015-09-18 13:55:18 -0400

    First Attempt

---
+++ assignment2.0/footer.php
@@ -0,0 +1,8 @@
+<!-- @@@@@@@@@@@@@@@@@@@@@@     Footer            @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+<footer>
+    <p>Web site designed by Preston Libby.</p>
+</footer>
+<!-- @@@@@@@@@@@@@@@@@@@@ Ends Footer             @@@@@@@@@@@@@@@@@@@@@@@@@@ -->
+
+</body>
+</html>
---
+++ assignment2.0/header.php
@@ -0,0 +1,12 @@
+<!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+
+<header>
+    <figure class="float_left small">
+            <img alt="Patrick Memeception" src="images/patrick.gif">
+            <figcaption>Hello...This is Patrick.</figcaption>
+    </figure>
+    <h1>CS148 Assignment 1.0</h1>
+    <h2>UVM Class Database Information</h2>
+</header>
+
+<!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
